c92c0de0b7a03e0cd51d7f044700dad7b249c033
Kotetsu Yamamoto <kotetsu.765@gmail.com>
2020-01-31 12:33:32 +0900

    change size

--- W25Q128_STM32F412ZG/Core/Src/Dev_Inf.c
+++ W25Q128_STM32F412ZG/Core/Src/Dev_Inf.c
@@ -13,7 +13,7 @@ struct StorageInfo const StorageInfo  =  {
    0x100,                    						// Programming Page Size 256Bytes
    0xFF,                       						// Initial Content of Erased Memory
 // Specify Size and Address of Sectors (view example below)
-   {{0x00000100, 0x00010000,},     				 		// Sector Num : 256 ,Sector Size: 64KBytes
+   {{0x00000010, 0x00010000,},     				 		// Sector Num : 10 ,Sector Size: 64KBytes
    {0x00000000, 0x00000000,},}
 }; 
 
